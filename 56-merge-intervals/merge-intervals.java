14c82590795dbe1fe68be74bdbf33b07d2677ff9
Ravi Kumar pal <rp077216@gmail.com>
2026-02-03 22:53:21 +0530

    Time: 9 ms (38.72%) | Memory: 49.2 MB (34.73%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,24 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<List<Integer>> list = new ArrayList<List<Integer>>();
-        int n = intervals.length;
-        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-
-        for (int i = 0; i < n; i++) {
-            if (list.isEmpty() || list.get(list.size() - 1).get(1) < intervals[i][0]) {
-                list.add(Arrays.asList(intervals[i][0], intervals[i][1]));
-            } else {
-                list.get(list.size() - 1).set(1,
-                    Math.max(list.get(list.size() - 1).get(1), intervals[i][1]));
+         // Sort intervals based on start time
+        Arrays.sort(
+            intervals,
+            (a, b) -> Integer.compare(a[0], b[0])
+        );
+        List<int[]> res = new ArrayList<>();
+        for(int[] interval : intervals ){
+            if(res.isEmpty() || res.get(res.size() - 1)[1] < interval[0]){
+                res.add(interval);
+            } else{
+                res.get(res.size() - 1)[1] =Math.max(res.get(res.size() - 1)[1], interval[1]);
             }
         }
 
-        // Convert List<List<Integer>> back to int[][]
-        int[][] result = new int[list.size()][2];
-        for (int i = 0; i < list.size(); i++) {
-            result[i][0] = list.get(i).get(0);
-            result[i][1] = list.get(i).get(1);
-        }
-        return result;
+        return res.toArray(new int[res.size()][2]);
+
     }
 }
