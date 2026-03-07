0202f4c5a767fbd0b71241a5beb1af576680044d
Ravi Kumar pal <rp077216@gmail.com>
2026-03-07 19:37:35 +0530

    Time: 26 ms (51.27%) | Memory: 47.5 MB (43.64%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,18 +1,29 @@
 class Solution {
     public int minFlips(String s) {
-        int n = s.length();
-        int res = n;
-        int[] op = {0, 0};
+       int n=s.length();
+       s=s+s;
+       char[]s1=new char[2*n];
+       char[]s2=new char[2*n];
+       for(int i=0;i<2*n;i++){
+            s1[i]=(i%2==0 ? '0' : '1');
+            s2[i]=(i%2==0? '1' : '0');
+       }
+       int flip1=0,flip2=0;
+       int i=0,j=0,ans=Integer.MAX_VALUE;
+       while(j<2*n){
+            if(s.charAt(j)!=s1[j]) flip1++;
+            if(s.charAt(j) !=s2[j]) flip2++;
 
-        for (int i = 0; i < n; i++)
-            op[(s.charAt(i) ^ i) & 1]++;
-
-        for (int i = 0; i < n; i++) {
-            op[(s.charAt(i) ^ i) & 1]--;
-            op[(s.charAt(i) ^ (n + i)) & 1]++;
-            res = Math.min(res, Math.min(op[0], op[1]));
-        }
-
-        return res;
+            if(j-i+1>n){
+                if(s.charAt(i) !=s1[i]) flip1--;
+                if(s.charAt(i) !=s2[i]) flip2--;
+                i++;
+            }
+            if(j-i+1==n){
+                ans=Math.min(ans,Math.min(flip1,flip2));
+            }
+            j++;
+       }
+       return ans;
     }
 }
