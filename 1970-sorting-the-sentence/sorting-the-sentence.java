1cf882a44c4ea461e7a2be3f5054213d78c423b3
Ravi Kumar pal <rp077216@gmail.com>
2026-03-18 23:30:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (11.57%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -1,34 +1,14 @@
 class Solution {
     public String sortSentence(String s) {
-        String[] arr = new String[10];
-		    StringBuilder sb = new StringBuilder();
-		    
-		    for (int i = 0; i < s.length(); i++) {
-		        char c = s.charAt(i);
-		        
-		        // 1. Check for letters (both cases)
-		        if (Character.isLetter(c)) {
-		            sb.append(c);
-		        } 
-		        // 2. Ignore spaces
-		        else if (c == ' ') {
-		            continue;
-		        } 
-		        // 3. If it's a digit, place the built word in the array
-		        else if (Character.isDigit(c)) {
-		            int idx = c - '0';
-		            arr[idx] = sb.toString();
-		            sb.setLength(0); // Clear StringBuilder for the next word
-		        }
-		    }
-		    for(int i=1;i<arr.length;i++) {
-		    	if(arr[i]!=null){
-                    if(sb.length()>0) sb.append(' ');
-                    sb.append(arr[i]);
-                }
-		    	    
-		    	
-		    }
-		    return sb.toString();
+
+        String[] words = s.split(" ");
+        String[] result = new String[words.length];
+
+        for(String word : words){
+            int pos = word.charAt(word.length() - 1) - '1';
+            result[pos] = word.substring(0, word.length() - 1);
+        }
+
+        return String.join(" ", result);
     }
 }
