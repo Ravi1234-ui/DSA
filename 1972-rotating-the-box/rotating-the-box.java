00a5154f1fc423957b81f2a5843652d5923a6369
Ravi Kumar pal <rp077216@gmail.com>
2026-05-06 21:47:33 +0530

    Time: 8 ms (59.38%) | Memory: 124.3 MB (94.47%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -0,0 +1,38 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+
+        // Step 1: Simulate gravity on each row
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
+
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (boxGrid[i][j] == '*') {
+                    empty = j - 1;
+                } 
+                else if (boxGrid[i][j] == '#') {
+
+                    // Move stone to the rightmost available position
+                    char temp = boxGrid[i][empty];
+                    boxGrid[i][empty] = '#';
+                    boxGrid[i][j] = temp;
+
+                    empty--;
+                }
+            }
+        }
+
+        // Step 2: Rotate matrix 90 degree clockwise
+        char[][] result = new char[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+
+        return result;
+    }
+}
